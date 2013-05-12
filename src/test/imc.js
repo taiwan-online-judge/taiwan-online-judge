bca5b722a36b941beba9daae96bf46ce97de8d88
pzread <netfirewall@gmail.com>
2013-05-12 23:59:58 +0800

    Done imc.js local <-> local

--- src/test/imc.js
+++ src/test/imc.js
@@ -7,25 +7,53 @@ var imc = new function(){
 
         var route_call = function(caller_retid,iden,dst,func_name,param){
             var i;
+            var part;
+            var dst_linkid;
+            var dst_path;
+            var caller_linkid;
+
+            var _retcall_cb = function(data){
+                func = caller_retidmap[caller_retid];
+                if(func == undefined){
+
+                }else{
+                    delete caller_retidmap[caller_retid]
+                    func({'stat':true,'data':data});
+                }
+            };
 
             part = dst.split('/');
-            dst_linkid = part[1];
-            dst_path = part.slice(2).join('/');
+            dst_linkid = part[2];
+            dst_path = part.slice(3).join('/');
 
             caller_linkid = iden.linkid
             if(caller_retid.split('/')[0] != caller_linkid){
                 return false;
             }
 
-            if(caller_linkid == linkid){
+            if(dst_linkid == linkid){
+                if(caller_linkid == linkid){
+                }else{
 
+                }
+
+                if((func = call_pathmap[dst_path + func_name]) == undefined){
+
+                }
+                func(param,_retcall_cb);
             }else{
 
             }
         };
 
         that.call = function(iden,dst,func_name,param,callback){
-            route_call(1234,iden,dst,func_name,param);
+            caller_retid = linkid + '/' + caller_retidmap.length;
+            caller_retidmap[caller_retid] = callback;
+            route_call(caller_retid,iden,dst,func_name,param);
+        };
+
+        that.register_call = function(path,func_name,func){
+            call_pathmap[path + func_name] = func;
         };
 
         imc.proxy.instance = that;
@@ -33,6 +61,9 @@ var imc = new function(){
 
 };
 
-var imc_call = function(iden,dst,func_name,param,callback){
-    imc.proxy.instance.call(iden,dst,func_name,param,callback) 
+function imc_call(iden,dst,func_name,param,callback){
+    imc.proxy.instance.call(iden,dst,func_name,param,callback); 
+};
+function imc_register_call(path,func_name,func){
+    imc.proxy.instance.register_call(path,func_name,func); 
 };
--- src/test/wstest.js
+++ src/test/wstest.js
@@ -2,17 +2,37 @@ var count = 0;
 var last = 0;
 var data = new ArrayBuffer(1024);
 
+var linkid = null;
+var iden = null;
+
 function test(){
     $.post('http://toj.tfcis.org:83/conn',{},function(res){
         var reto;
         
         if(res[0] != 'E'){
             reto = JSON.parse(res)
+            linkid = reto.client_linkid;
+
+            new imc.proxy(linkid);
+            iden = {'linkclass':'client','linkid':linkid};
+
+            imc_register_call('','test_dst',test_dst);
+    
+            imc_call(iden,'/client/test/','test_dst','Hello',function(result){
+                console.log(result);
+            });
+
             conn(reto.client_linkid,reto.worker_linkid,reto.ip,reto.port);
         }else{
 
         }
     });
+}
+
+function test_dst(param,callback){
+    console.log(param);
+    
+    callback('Hello Too');
 }
 
 function conn(client_linkid,worker_linkid,ip,port){
@@ -24,7 +44,7 @@ function conn(client_linkid,worker_linkid,ip,port){
     var imc_call = function(iden,dst,func_name,param){
         call = {
             'type':'call',
-            'caller_retid':client_linkid + '/' + 'genid_13',
+            'caller_retid':client_linkid + '/' + '13',
             'timeout':60000,
             'iden':iden,
             'dst':dst,
@@ -40,22 +60,27 @@ function conn(client_linkid,worker_linkid,ip,port){
     ws.onopen = function(){
         var i;
 
+        $('div.main > div.error > div.reconnect').alert('close');
+
         console.log('open');
         ws.send(JSON.stringify({
             'client_linkid':client_linkid
         }))
 
-        imc_call(client_linkid,'/backend/' + worker_linkid,'test_dst','Hello');
+        imc_call({'linkclass':'client','linkid':client_linkid},'/center/1' + '/','test_dst','');
     };
     ws.onmessage = function(e){
         reader.onload = function(e){
             var res = e.target.result;    
-            //console.log(JSON.parse(res));
+            console.log(JSON.parse(res));
 
-            imc_call(client_linkid,'/backend/' + worker_linkid,'test_dst','Hello');
+            //imc_call({'linkclass':'client','linkid':client_linkid},'/center/' + worker_linkid + '/','test_dst','Hello');
         };
         reader.readAsText(e.data);
         count++;
+    };
+    ws.onclose = function(e){
+        console.log('close');
     };
 
     perf();
